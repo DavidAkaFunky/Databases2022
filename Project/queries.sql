d54fc86dc5418ba47097008ebb3947b659787f2d
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2022-06-21 04:10:09 +0100

    One more query added

--- Project/queries.sql
+++ Project/queries.sql
@@ -1,10 +1,33 @@
 /*Qual o nome do retalhista (ou retalhistas) responsaveis pela reposicao do maior numero de
 categorias? */
 
-SELECT nome
+SELECT retalhista.nome
 FROM retalhista NATURAL JOIN evento_reposicao NATURAL JOIN responsavel_por
 GROUP BY tin
 HAVING COUNT(*) >= ALL
     (SELECT COUNT(*)
     FROM retalhista NATURAL JOIN evento_reposicao NATURAL JOIN responsavel_por
     GROUP BY tin);
+
+/*Qual o nome do ou dos retalhistas que são responsáveis por todas as categorias simples?*/
+
+
+
+
+
+/*Quais os produtos (ean) que nunca foram repostos?*/
+
+SELECT produto.ean
+FROM produto
+WHERE produto.ean
+NOT IN(
+	SELECT evento_reposicao.ean
+	FROM evento_reposicao
+) 
+
+/*Quais os produtos (ean) que foram repostos sempre pelo mesmo retalhista?*/
+
+
+
+
+
