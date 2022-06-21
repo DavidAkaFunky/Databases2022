29cc20fbadd908b83c89ef5080e6717c46a7bdc4
FunkyCracky <futebol4@hotmail.com>
2022-06-21 13:52:43 +0100

    View attempt #2 (seems good now)

--- Project/views.sql
+++ Project/views.sql
@@ -1,8 +1,8 @@
 CREATE VIEW vendas(ean, cat, ano, trimestre, dia_mes, dia_semana, distrito, concelho, unidades) AS
-    SELECT ean, name AS cat, EXTRACT(YEAR from instante) AS ano, EXTRACT(QUARTER from instante) AS trimestre, EXTRACT(DAY from instante) AS dia_mes, EXTRACT(WEEKDAY from instante) AS dia_semana, distrito, concelho, unidades
+    SELECT ean, nome AS cat, EXTRACT(YEAR from instante) AS ano, EXTRACT(QUARTER from instante) AS trimestre, EXTRACT(DAY from instante) AS dia_mes, EXTRACT(WEEKDAY from instante) AS dia_semana, distrito, concelho, unidades
     FROM (SELECT ean, nro, num_serie, fabricante, instante, unidades
-          FROM evento_reposicao)
+          FROM evento_reposicao) AS evento_reposicao
     NATURAL JOIN tem_categoria
     NATURAL JOIN instalada_em
     NATURAL JOIN (SELECT nome AS local, distrito, concelho
-                  FROM ponto_de_retalho)
+                  FROM ponto_de_retalho) AS ponto_de_retalho;
